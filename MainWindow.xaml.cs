1fe58b60dc0f2d664bbafd4cd628dd164d081551
Полина Мелинти <108030042+Polointhebegining@users.noreply.github.com>
2024-01-10 12:26:22 +0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows;
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        this.DataContext = new YourViewModel();
+    }
+}
+
---
+++ YourViewModel.cs
@@ -0,0 +1,70 @@
+using Lab2;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Input;
+public class YourViewModel : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    TodoTask selectedTask;
+    protected virtual void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    public ObservableCollection<TodoTask> TodoList { get; set; }
+    public ICommand EditTaskCommand { get; private set; }
+    public ICommand NewTaskCommand { get; private set; }
+
+
+    public TodoTask SelectedTask
+    {
+        get { return selectedTask; }
+        set
+        {
+            if (selectedTask != value)
+            {
+                selectedTask = value;
+                OnPropertyChanged(nameof(SelectedTask));
+            }
+        }
+    }
+
+    public YourViewModel()
+    {
+        TodoList = new ObservableCollection<TodoTask>();
+        EditTaskCommand = new RelayCommand(EditTask);
+        NewTaskCommand = new RelayCommand(CreateNewTask);
+    }
+
+    private void EditTask(object parameter)
+    {
+        if (SelectedTask != null)
+        {
+            var editWindow = new TaskDialogWindow();
+            editWindow.DataContext = SelectedTask.Clone(); // Тут нужно  клонирование для избежания изменений в оригинале
+            var result = editWindow.ShowDialog();
+
+            if (result == true)
+            {
+                // Тут обновляем TodoList и соответствующий объект TodoTask
+                int index = TodoList.IndexOf(SelectedTask);
+                TodoList[index] = (TodoTask)editWindow.DataContext;
+                OnPropertyChanged(nameof(TodoList));
+            }
+        }
+    }
+
+    private void CreateNewTask(object parameter)
+    {
+        var newTaskWindow = new TaskDialogWindow();
+        var result = newTaskWindow.ShowDialog();
+
+        if (result == true)
+        {
+            TodoList.Add((TodoTask)newTaskWindow.DataContext);
+            OnPropertyChanged(nameof(TodoList));
+        }
+    }
+}
